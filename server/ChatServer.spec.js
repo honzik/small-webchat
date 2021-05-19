c71be880aacc8fe8c2b09b7911b8725117c3431e
Honza Maly <honzamaly@gmail.com>
2021-05-19 15:22:25 +0200

    Added more tests

--- server/ChatServer.spec.js
+++ server/ChatServer.spec.js
@@ -8,22 +8,41 @@ class WebSocketServerMock {
 
 const wss = new WebSocketServerMock;
 
+class WebSocket {
+    constructor() {
+        this.send = jest.fn((message) => {});
+        this.on = jest.fn((event, method) => {})
+    }
+}
+
+const ws = new WebSocket;
+
 
 describe("ChatServer Test Suite", () => {
 
     beforeEach(()=>{
         wss.on.mockClear();        
+        ws.on.mockClear();
+        ws.send.mockClear();
     })
 
     test("inits properly", () => {
-        const cs = new ChatServer(wss);
-        const onMock = wss.on;
+        const cs = new ChatServer(wss);        
         expect(cs.clientList.length).toEqual(0);
-        expect(onMock).toHaveBeenCalledTimes(1);
-        expect(onMock.mock.calls[0][0]).toEqual("connection");
-        expect(typeof onMock.mock.calls[0][1]).toEqual("function");
+        expect(wss.on).toHaveBeenCalledTimes(1);
+        expect(wss.on.mock.calls[0][0]).toEqual("connection");
+        expect(typeof wss.on.mock.calls[0][1]).toEqual("function");
     });
 
-    
-
+    test("incoming connection handling", () => {
+        const cs = new ChatServer(wss);
+        
+        // simulate connection
+        cs.connectHandler(ws);        
+        expect(cs.clientList.length).toEqual(1);
+        expect(cs.clientList[0].nickname).toBeDefined();
+        expect(cs.clientList[0].ws).toEqual(ws);
+        expect(ws.on).toHaveBeenCalledTimes(2);        
+        expect(ws.send).toHaveBeenCalledTimes(2);
+    });
 });
